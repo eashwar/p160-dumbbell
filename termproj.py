4445df3313fbbcef10945aa4b156485c097988ec
Eashwar Mohan <luigi629@gmail.com>
2017-12-06 19:35:53 -0500

    wrote more code

--- termproj.py
+++ termproj.py
@@ -10,12 +10,12 @@ exa = 1e18 ## using this for masses
 # CUSTOMIZATION
 
 ## Scene Parameters
-sceneWidth = 1200
-sceneAspect = 1/1
-scene = display(x=10, y=50, width=sceneWidth, height=sceneWidth*sceneAspect, background=color.white)
+sceneHeight = 900
+sceneAspect = 16/9
+scene = display(x=10, y=50, width=sceneHeight*sceneAspect, height=sceneHeight, background=color.white)
 dumbbellColor = vector(.4,.4,.4)
-tightColor = vector(.5 + random.random()*.5,0,.5 + random.random())
-looseColor = vector(0,.5 + random.random()*.5,5 + random.random())
+tightColor = vector(.5 + random.random()*.5,0,.5 + random.random()) # random purple
+looseColor = vector(0,.5 + random.random()*.5,5 + random.random()) # random cyan
 
 
 ## System Parameters
@@ -24,11 +24,13 @@ orbitingRad = 1*mega #1000 kilometers
 
 rodLength = 40*mega  #40000 kilometers
 
-dumbbellMass = 200*exa #5e19 kilograms
+dumbbellMass = 200*exa #2e20 kilograms
 tightMass = 1*exa     #1e18 kilograms
-looseMass = 5*exa     #5e18 kilograms
+looseMass = 10*exa     #5e18 kilograms
 
-dumbbellVelocity = vector(0,0,0)
+y_t_init = 20*mega
+y_l_init = 40*mega
+
 
 # 3D MODELLING
 
@@ -39,7 +41,7 @@ end_one = sphere(pos=(-20*mega,0,0),
                  radius=dumbbellRad,
                  mass=dumbbellMass,
                  color=dumbbellColor)
-end_one.velocity = dumbbellVelocity
+end_one.velocity = vector(0,0,0)
 end_one.momentum = end_one.mass * end_one.velocity
 
 ### End Two
@@ -55,7 +57,7 @@ com = sphere(pos=(0,0,0),
              radius=1.3*mega,
              color=color.black)
 
-### Moment Calculation
+### Moment Calculation using parallel axis theorem
 moment_one = .4*end_one.mass*(end_one.radius**2) + end_one.mass*mag2(com.pos-end_one.pos)
 moment_two = .4*end_two.mass*(end_two.radius**2) + end_two.mass*mag2(com.pos-end_two.pos)
 net_moment = moment_one+moment_two
@@ -74,16 +76,16 @@ dumbbell_angle = 0
 
 ## Orbiting bodies
 
-tight = sphere(pos=(-20*mega, 30*mega, 0),
+tight = sphere(pos=(-20*mega, y_t_init, 0),
                  radius=1*mega, 
                  mass=1*exa,
                  color=tightColor, make_trail=True)
 
-tight.velocity = vector(0,0,0)
+tight.velocity = vector(0,0,20)
 tight.momentum = tight.mass * tight.velocity
 
 
-loose = sphere(pos=(0, 40*mega, 0),
+loose = sphere(pos=(0, y_l_init, 0),
                 radius=1*mega,
                 mass=5*exa,
                 color=looseColor, make_trail=True)
@@ -91,14 +93,14 @@ loose = sphere(pos=(0, 40*mega, 0),
 loose.velocity = vector(0,0,7)
 loose.momentum = loose.mass * loose.velocity
 
-## TIME
+# TIME
 t = 0
 dt = 100
    
 while True:
     rate(10000)
 
-    #FORCE CALCULATION
+    #FORCE AND TORQUE CALCULATION
     
     ## Tightly Orbiting Body
     F_tight_one = -1 * norm(tight.pos-end_one.pos) * G * tight.mass * end_one.mass / mag2(tight.pos-end_one.pos)
@@ -111,18 +113,28 @@ while True:
     F_loose_two = -1 * norm(loose.pos-end_two.pos) * G * loose.mass * end_two.mass / mag2(loose.pos-end_two.pos)
     F_loose_tight = -1 * norm(loose.pos-tight.pos) * G * loose.mass * tight.mass / mag2(loose.pos-tight.pos)
     F_loose_net = F_loose_one + F_loose_two + F_loose_tight
-    
-    ## End One
+
+    ## Dumbbell
+
+    ### End One
     F_one_tight = -1 * norm(end_one.pos-tight.pos) * G * end_one.mass * tight.mass / mag2(end_one.pos-tight.pos)
     F_one_loose = -1 * norm(end_one.pos-loose.pos) * G * end_one.mass * loose.mass / mag2(end_one.pos-loose.pos)
     F_one_net = F_one_tight + F_one_loose
+    moment_one = .4*end_one.mass*(end_one.radius**2) + end_one.mass*mag2(com.pos-end_one.pos)
+    T_one = cross(F_one_net, end_one.pos-com.pos)
 
-    ## End Two
+
+    ### End Two
     F_two_tight = -1 * norm(end_two.pos-tight.pos) * G * end_two.mass * tight.mass / mag2(end_two.pos-tight.pos)
     F_two_loose = -1 * norm(end_two.pos-loose.pos) * G * end_two.mass * loose.mass / mag2(end_two.pos-loose.pos)
     F_two_net = F_two_tight + F_two_loose
+    moment_two = .4*end_two.mass*(end_two.radius**2) + end_two.mass*mag2(com.pos-end_two.pos)    
+    T_two = cross(F_two_net, end_two.pos-com.pos)
 
+    ### The entire dumbbell
     F_dumbbell = F_one_net + F_two_net
+    rod.moment = moment_one+moment_two
+    T_net = T_one + T_two
 
 
     #MOMENTUM PRINCIPLE
@@ -138,7 +150,8 @@ while True:
     loose.pos += loose.velocity*dt
 
     ## Dumbbell
-    ### Linear Momentum Principle
+    
+    ### Linear
     com.pos = ((end_one.pos*end_one.mass) + (end_two.pos*end_two.mass))/(end_one.mass+end_two.mass)
     end_one.momentum += F_dumbbell*dt
     end_one.velocity = end_one.momentum/end_one.mass
@@ -150,19 +163,11 @@ while True:
     end_two.velocity = end_two.momentum/end_two.mass
     end_two.pos += end_two.velocity*dt
     
-    ### Angular Momentum Principle
-    moment_one = .4*end_one.mass*(end_one.radius**2) + end_one.mass*mag2(com.pos-end_one.pos)
-    moment_two = .4*end_two.mass*(end_two.radius**2) + end_two.mass*mag2(com.pos-end_two.pos)
-    rod.moment = moment_one+moment_two
-
-    T_one = cross(F_one_net, end_one.pos-com.pos)
-    T_two = cross(F_two_net, end_two.pos-com.pos)
-    T_net = T_one + T_two
-    
+    ### Angular
     rod.ang_mom += T_net*dt
     rod.ang_vel = rod.ang_mom/rod.moment
 
-    dumbbell_angle += mag(rod.ang_vel)*dt
+    dumbbell_angle = -mag(rod.ang_vel)*dt
     
     rod.rotate(angle=dumbbell_angle, axis=rod.ang_vel, origin=com.pos)
     end_one.rotate(angle=dumbbell_angle, axis=rod.ang_vel, origin=com.pos)
